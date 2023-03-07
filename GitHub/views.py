a88f4bf3809111ea5d148f3367598e2e2173e6f4
ManoharGoud2000 <manohar143goud@gmail.com>
2023-03-07 16:01:26 +0530

    first commit

---
+++ GitHub/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def GitHub(request):
+    return HttpResponse('<marquee><h1>GitHub is running Succesfully</h1></marquee>')
